6bc56244d6dadea20ab57e2685277a8b49a4da47
eenticott <43878650+eenticott@users.noreply.github.com>
2026-01-15 23:04:35 +0000

    Added data documentation

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' French Electricity Spot Prices
+#'
+#' Contains historical spot prices for electricity in France as well as generation mix and consumption data.
+#'
+#' @format A data frame with 34896 rows and 113 variables:
+#' \describe{
+#'   \item{Date}{Date of the observation}
+#'   \item{Hour}{Hour of the observation}
+#'   \item{SpotPrice}{Spot price in EUR/MWh}
+#' }
+#' @source \url{https://www.entsoe.eu/}
+#' @source \url{https://odre.opendatasoft.com/explore/dataset/eco2mix-national-cons-def}
+"fr_electricity_price"
